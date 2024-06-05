f7d1212f4f92b40dbc3d16c4cf16e359381317f2
Andy Ramaroson <andyramaroson@gmail.com>
2024-06-05 22:59:21 +0200

    (feat) update contentlayer model add project

--- contentlayer.config.js
+++ contentlayer.config.js
@@ -2,69 +2,122 @@ import {
   defineNestedType,
   defineDocumentType,
   makeSource,
-} from 'contentlayer/source-files';
+} from 'contentlayer/source-files'
 
 const Post = defineDocumentType(() => ({
-  name: "Post",
+  name: 'Post',
   filePathPattern: `blog/*.mdx`,
-  contentType: "mdx",
+  contentType: 'mdx',
   fields: {
     title: {
-      type: "string",
-      description: "the title of the post ",
+      type: 'string',
+      description: 'the title of the post ',
       required: true,
     },
     excerpt: {
-      type:"string",
-      description: "the description of the post ",
+      type: 'string',
+      description: 'the description of the post ',
       required: true,
     },
     date: {
-      type: "date",
-      description: "the date of the post ",
+      type: 'date',
+      description: 'the date of the post ',
       required: true,
     },
     image: {
-      type: "string",
-      description: "the  featured image of the post ",
+      type: 'string',
+      description: 'the  featured image of the post ',
       required: true,
     },
     author: {
-      type: "string",
-      description: "the  author image of the post ",
+      type: 'string',
+      description: 'the  author image of the post ',
       required: true,
     },
     categories: {
-      type: "list",
+      type: 'list',
       of: Category,
-      description: "the  categories image of the post ",
+      description: 'the  categories image of the post ',
       required: true,
     },
   },
   computedFields: {
     url: {
-      type: "string",
+      type: 'string',
       resolve: (doc) => `${doc._raw.flattenedPath}`,
-    }
-  }
+    },
+  },
+}))
+
+const Project = defineDocumentType(() => ({
+  name: 'Project',
+  filePathPattern: `projects/*.mdx`,
+  contentType: 'mdx',
+  fields: {
+    title: {
+      type: 'string',
+      description: 'The title of the project',
+      require: true,
+    },
+    location: {
+      type: 'string',
+      description: 'The location of the project',
+      require: true,
+    },
+    date: {
+      type: 'date',
+      description: 'The date of the project',
+      require: true,
+    },
+    image: {
+      type: 'string',
+      description: 'The image of the project',
+      require: true,
+    },
+    client: {
+      type: 'string',
+      description: 'The client of the project',
+      require: true,
+    },
+    role: {
+      type: 'string',
+      description: 'The role of the project',
+      require: true,
+    },
+    year: {
+      type: 'string',
+      description: 'The year of the project',
+      require: true,
+    },
+    description: {
+      type: 'string',
+      description: 'the  description content of the project ',
+      required: true,
+    },
+  },
+  computedFields: {
+    url: {
+      type: 'string',
+      resolve: (doc) => `/${doc._raw.flattenedPath}`,
+    },
+  },
 }))
 
 const Category = defineNestedType(() => ({
-  name: "Category",
+  name: 'Category',
   fields: {
     title: {
-      type: "string",
-      description: "The title of the category",
+      type: 'string',
+      description: 'The title of the category',
       required: true,
-    }
-  }
+    },
+  },
 }))
 
-
 const Info = defineDocumentType(() => ({
-  name: "Info",
+  name: 'Info',
   filePathPattern: `privacies/*.mdx`,
-  contentType: "mdx",
+  contentType: 'mdx',
   fields: {
     title: {
       type: 'string',
@@ -79,13 +132,13 @@ const Info = defineDocumentType(() => ({
   },
   computedFields: {
     url: {
-      type: "string",
+      type: 'string',
       resolve: (doc) => `${doc._raw.flattenedPath}`,
-    }
-  }
+    },
+  },
 }))
 
 export default makeSource({
-  contentDirPath: "content",
-  documentTypes: [Post, Info],
+  contentDirPath: 'content',
+  documentTypes: [Post, Info, Project],
 })
