4ab6bf78f45fc8682cf97f35441740f6d357652f
andy ramaroson <andyramaroson@gmail.com>
2025-04-15 11:18:25 +0200

    [fix] path Footer

--- app/components/Footer.jsx
+++ app/components/Footer.jsx
@@ -8,7 +8,7 @@ import { BiSolidHeart } from 'react-icons/bi'
 import { FaGithub } from 'react-icons/fa'
 import IconLinkedin from './../../public/images/linkedin.svg'
 import IconMalt from './../../public/images/malt.svg'
-
+import IconLogo from './../../public/images/logo.svg' 
 
 const footerContent = {
   footerLinks: [
@@ -28,7 +28,6 @@ const footerContent = {
           href: '/contact',
           label: 'Contact',
         },
-
       ],
     },
     {
@@ -104,21 +103,43 @@ const Copyright = () => {
 
 const Footer = ({ className }) => {
   return (
-    <footer className={`${className} overflow-hidden w-full h-full z-[2]`}>
-      <div className="container mx-auto px-4 relative mt-10">
-        <div className="md:flex ">
-          <div className="md:w-4/12 mb-5 md:mb-0">
-            <Link href="#" className="text-[28px] text-[#2F2E2E] font-bold">
-              Andy Ramaroson
-              <span className="text-[#2F2E2E]">.</span>
-            </Link>
+    <footer className={`${className} overflow-hidden w-full h-full z-[2] `}>
+      <div className="container mx-auto px-4 relative mt-8">
+        <div className="lg:flex">
+          <div className="lg:w-4/12"> 
+            <div className="mb-6 md:mb-0">
+              <Link href="/" className="flex flex-row text-2xl font-bold"> 
+                {/* Logo - Navigation - left */}
+                <span className="pt-2 py-4 pr-2">
+                  <Image src={IconLogo} width={60} height={60} alt="logo" /> 
+                </span>
+                <span className="text-red-500">
+                  Lemurian Agency
+                </span>
+              </Link>
+
+              <p className="text-start text-black">
+                60 rue François 1er <br className="hidden md:block" />
+                75008 Paris
+              </p>
+              <p className="text-start text-black">
+                plomberie.depannage33@gmail.com
+              </p>
+              <p className="text-start text-black font-bold">
+                {' '}
+                05.56.34.89.79 -{' '}
+                <span className="text-red-500 font-bold text-center">
+                  06.56.34.89.79
+                </span>
+              </p>
+            </div>
             <br />
             <div className="flex mt-2">
               <span className="flex flex-row">
                 <FaGithub />
                 <Link
                   href="https://github.com/AndyRama"
-                  className="text-[12px] text-[#2F2E2E] hover:text-[#ff5e29] font-bold ml-2 mr-5 mt-[-2]"
+                  className="text-[12px] text-[#2F2E2E] hover:text-[#ff5e29] font-bold ml-2 mr-5 mt-[-2px]" 
                   alt="andy rama Github link"
                   target="_blank"
                 >
@@ -128,13 +149,13 @@ const Footer = ({ className }) => {
               <span className="flex flex-row text-center">
                 <Image
                   src={IconLinkedin}
-                  weight={18}
+                  width={18} 
                   height={18}
                   alt="logo linkedin"
                 />
                 <Link
                   href="http://www.linkedin.com/in/andy-ramaroson"
-                  className="text-[12px] text-[#2F2E2E] hover:text-[#ff5e29] font-bold ml-2 mr-5 mt-[-2]"
+                  className="text-[12px] text-[#2F2E2E] hover:text-[#ff5e29] font-bold ml-2 mr-5 mt-[-2px]"
                   alt="andy rama Linkedin link"
                   target="_blank"
                 >
@@ -142,10 +163,15 @@ const Footer = ({ className }) => {
                 </Link>
               </span>
               <span className="flex flex-row text-center">
-                <Image src={IconMalt} weight={18} height={18} alt="logo malt" />
+                <Image 
+                  src={IconMalt} 
+                  width={18} 
+                  height={18} 
+                  alt="logo malt" 
+                />
                 <Link
                   href="#"
-                  className="text-[12px] text-[#2F2E2E] hover:text-[#ff5e29] font-bold ml-2 mt-[-2]"
+                  className="text-[12px] text-[#2F2E2E] hover:text-[#ff5e29] font-bold ml-2 mt-[-2px]"
                   alt="andy rama Linkedin link"
                   target="_blank"
                 >
@@ -155,8 +181,8 @@ const Footer = ({ className }) => {
             </div>
           </div>
 
-          <div className="md:w-8/12 ">
-            <div className="grid grid-cols-2 md:grid-cols-4 gap 0 md:gap-4 ">
+          <div className="md:w-8/12">
+            <div className="grid grid-cols-2 md:grid-cols-4 gap-4"> 
               {footerContent.footerLinks.map((item, index) => (
                 <div className="mb-5 md:mb-0" key={item.heading}>
                   <h3 className="text-gray-400 mb-3">{item.heading}</h3>
@@ -182,7 +208,18 @@ const Footer = ({ className }) => {
                               </span>
                             </>
                           ) : (
-                            link.label
+                            <>
+                              <span>{link.label}</span>
+                              {link.badge && (
+                                <span
+                                  className="py-0.5 px-1 rounded-full bg-orange-100
+                                      border border-orange-500 text-[10px] text-[#ff5e29]
+                                      invisible sm:visible"
+                                >
+                                  {link.badge}
+                                </span>
+                              )}
+                            </>
                           )}
                         </Link>
                       </li>
@@ -214,7 +251,7 @@ const Footer = ({ className }) => {
 
         <Copyright />
       </div>
-      <div className="transform-gpu blur-3xl ">
+      <div className="transform-gpu blur-3xl">
         <div className="absolute -mt-20 left-0 opacity-50 -z-50 w-32 h-48 md:w-80 md:h-96 bg-gradient-to-tr from-orange-300 to-orange-500"></div>
         <div className="absolute -mt-20 right-0 opacity-0 md:opacity-50 -z-50 w-80 h-96 bg-gradient-to-tr from-orange-300 to-orange-500"></div>
       </div>
